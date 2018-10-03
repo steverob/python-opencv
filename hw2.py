cd96f6d3161ff452368eb704563aac59d0d72dc7
Steve Robinson <steve.rob@me.com>
2018-10-03 15:09:14 +0530

    modified reload_image

--- hw2.py
+++ hw2.py
@@ -24,15 +24,14 @@ def load_image():
     cv2.imshow('image', img)
     return (img, file_name)
 
-def reloadimage(filename):
-    if len(filename) > 1:
-        original_image = cv2.imread(filename)
+def reload_image(file_name):
+    if not file_name.strip():
+        capturer = cv2.VideoCapture(0)
+        _, img = capturer.read()
     else:
-        cap = cv2.VideoCapture(0)
-        retval,original_image = cap.read()
-    while original_image.shape[0] > 1200 or original_image.shape[1] > 750:
-        original_image = cv2.resize(original_image,(int(original_image.shape[1]/2), int(original_image.shape[0]/2)))
-    return original_image
+        img = cv2.imread(file_name)
+    
+    return img
 
 def togray():
     global image
@@ -42,7 +41,7 @@ def togray():
 def sliderHandler(self):
     global image
     n = self
-    image = reloadimage(filename)
+    image = reload_image(filename)
     image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     if self != 0:
         kernel = np.ones((n,n), np.float32)/(n*n)
@@ -75,7 +74,7 @@ def sliderHandler2(self):
 
 def sliderHandler3(self):
     global image
-    image = reloadimage(filename)
+    image = reload_image(filename)
     image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     new = image
     n = self
@@ -94,7 +93,7 @@ def sliderHandler3(self):
 
 def sliderHandler4(self):
     global image
-    image = reloadimage(filename)
+    image = reload_image(filename)
     image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     new = image
     n = self
@@ -115,17 +114,17 @@ def main():
         key = cv2.waitKey()
         print (key)
         if key == ord('i'):
-            image = reloadimage(filename)
+            image = reload_image(filename)
 
         elif key == ord('w'):
             cv2.imwrite("out.jpg", image)
 
         elif key == ord('g'):
-            image = reloadimage(filename)
+            image = reload_image(filename)
             image = togray()
 
         elif key == ord('G'):
-            image = reloadimage(filename)
+            image = reload_image(filename)
             if len(image.shape) == 3:
                 aux = np.zeros((image.shape[0], image.shape[1]), dtype = image.dtype)
                 for (x, y), v in np.ndenumerate(aux):
@@ -135,7 +134,7 @@ def main():
                 image_gray = image
 
         elif key == ord('c'):
-            image = reloadimage(filename)
+            image = reload_image(filename)
             if len(image.shape) == 3:
                 if count == 0:
                     image[:,:,1] = 0
@@ -153,39 +152,39 @@ def main():
                 print("Not possible to convert to b, g or r")
 
         elif key == ord('s'):
-           image = reloadimage(filename)
+           image = reload_image(filename)
            image = togray()
            cv2.imshow('image', image)
            cv2.createTrackbar('s', 'image', 0, 255, sliderHandler)
 
         elif key == ord('S'):
-            image = reloadimage(filename)
+            image = reload_image(filename)
             image = togray()
             cv2.imshow('image', image)
             cv2.createTrackbar('s', 'image', 0, 255, sliderHandler2)
 
         elif key == ord('d'):
-            image = reloadimage(filename)
+            image = reload_image(filename)
             image = cv2.resize(image, (int(image.shape[1]/2), int(image.shape[0]/2)))
 
         elif key == ord('D'):
-            image = reloadimage(filename)
+            image = reload_image(filename)
             image = cv2.pyrDown(image)
 
         elif key == ord('x'):
-            image = reloadimage(filename)
+            image = reload_image(filename)
             image = togray()
             sobelx = cv2.Sobel(image,cv2.CV_64F,1,0,ksize=5)
             image = cv2.normalize(sobelx, image, alpha = 0, beta = 1,norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_64F)
 
         elif key == ord('y'):
-            image = reloadimage(filename)
+            image = reload_image(filename)
             image = togray()
             sobely = cv2.Sobel(image,cv2.CV_64F,0,1,ksize=5)
             image = cv2.normalize(sobely, image, alpha = 0, beta = 1,norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_64F)
 
         elif key == ord('m'):
-            image = reloadimage(filename)
+            image = reload_image(filename)
             image = togray()
             sobelx = cv2.Sobel(image,cv2.CV_64F,1,0,ksize=5)
             sobely = cv2.Sobel(image,cv2.CV_64F,0,1,ksize=5)
@@ -193,12 +192,12 @@ def main():
             image = cv2.normalize(gradient, image, alpha = 0, beta = 1,norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_64F)
 
         elif key == ord('p'):
-            image = reloadimage(filename)
+            image = reload_image(filename)
             image = togray()
             cv2.createTrackbar('s', 'image', 0,255, sliderHandler3)
 
         elif key == ord('r'):
-            image = reloadimage(filename)
+            image = reload_image(filename)
             image = togray()
             cv2.createTrackbar('s', 'image', 0, 360, sliderHandler4)
 
